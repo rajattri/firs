e86d108372a17a63e236e79c63dbde662b4b8792
rajattri <rajattripathi19@gmail.com>
2018-10-30 16:56:40 +0530

    Third commit

--- helloworld.c
+++ helloworld.c
@@ -4,5 +4,6 @@ int main()
 {
 	printf("Hello fellow inmates! Lets get outta here");
         printf("I am your leader and you must pay your fielty to me");
+        printf("Gimme your blood and take I will give you freedom");
 return;
 }
